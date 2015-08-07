4b498bc62578f8009b46a71ca59d74318a25d84b
John Papa <john@johnpapa.net>
2015-08-06 22:16:08 -0400

    updated config.js for systemjs

--- config.js
+++ config.js
@@ -1,13 +1,15 @@
 System.config({
-  "baseURL": "/dist",
-  "transpiler": "babel",
-  "babelOptions": {
-    "optional": [
-      "runtime"
-    ]
-  },
-  "paths": {
-    "*": "*.js",
-  },
-  "defaultJSExtensions": true
+    "baseURL": "/dist",
+    "transpiler": "babel",
+    "babelOptions": {
+        "optional": [
+            "es7.decorators",
+            "es7.classProperties",
+            "runtime"
+        ]
+    },
+    "paths": {
+        "*": "*",
+    },
+    "defaultJSExtensions": true
 });
